1f7f8994cf7f2eb73eb3a7c6d39ec0742bf9e145
Yellow_Ledbetter <stuti_v@protonmail.com>
2021-07-01 11:05:04 +0530

    product cards

--- pages/index.js
+++ pages/index.js
@@ -13,41 +13,38 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Neat Poster Shop
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          The sleekest posters on the internet.
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Helvetica Typography</h2>
+            <p>This is a dummy statement that will be replaced later.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Uno Theme</h2>
+            <p>This is a dummy statement that will be replaced later.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Online - Offline</h2>
+            <p>This is a dummy statement that will be replaced later.</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>Do Something</h2>
+            <p>This is a dummy statement that will be replaced later.</p>
           </a>
         </div>
       </main>
